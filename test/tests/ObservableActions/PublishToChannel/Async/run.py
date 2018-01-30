b1b165ef3fa1e1a23b71304032db95910ed9b4c4
RPEA <richard.peach@softwareag.com>
2018-01-30 22:27:34 +0000

    Adding every and contains and adding tests for and fixing bugs in publishTo, receiveFrom, observeOn

---
+++ test/tests/ObservableActions/PublishToChannel/Async/run.py
@@ -0,0 +1,41 @@
+# Sample PySys testcase
+# Copyright (c) 2015-2016 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates and/or their licensors. 
+# Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided for in your License Agreement with Software AG 
+
+from pysys.constants import *
+from pysys.basetest import BaseTest
+from apama.correlator import CorrelatorHelper
+
+class PySysTest(BaseTest):
+
+	def execute(self):
+		correlator = CorrelatorHelper(self, name='correlator')
+		
+		correlator.start(logfile='correlator.log', config=os.path.join(PROJECT.TEST_SUBJECT_DIR,'initialization.yaml'))
+		correlator.injectEPL(filenames='testUtils.mon', filedir=PROJECT.UTILS_DIR)
+		
+		# Start test results receiver
+		correlator.receive(filename='TestResult.evt', channels=['TestResult'], logChannels=True)
+		
+		# Set the log level to DEBUG so that we can see when the listeners are killed
+		correlator.setApplicationLogLevel(verbosity='DEBUG')
+				
+		# Inject test
+		correlator.injectEPL(filenames=['test.mon'])
+		
+		# wait for all events to be processed
+		correlator.flush()
+		
+		# wait for test to complete
+		self.waitForSignal('TestResult.evt', expr="TestComplete", condition="==1", timeout=3)
+		
+	def validate(self):
+		# check the main correlator log for Errors
+		self.assertGrep('correlator.log', expr=' (ERROR|FATAL) ', contains=False)
+		
+		# Check that the test didn't fail
+		self.assertGrep('TestResult.evt', expr='TestFailed', contains=False)
+		
+		# Check that the publishToChannel was killed
+		self.assertLineCount('correlator.log', expr='Tearing down PublishTo', condition='==1')
+		
