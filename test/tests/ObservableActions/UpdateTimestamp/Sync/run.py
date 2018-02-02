8bcbe16ff2fd6c8b0f5625fe8e0ccbdf4d7b1497
RPEA <richard.peach@softwareag.com>
2018-02-02 20:55:15 +0000

    Adding timestamp and updateTimestamp

---
+++ test/tests/ObservableActions/UpdateTimestamp/Sync/run.py
@@ -0,0 +1,37 @@
+# Sample PySys testcase
+# Copyright (c) 2015-2016 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates and/or their licensors. 
+# Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided for in your License Agreement with Software AG 
+
+from pysys.constants import *
+from pysys.basetest import BaseTest
+from apama.correlator import CorrelatorHelper
+
+class PySysTest(BaseTest):
+
+	def execute(self):
+		correlator = CorrelatorHelper(self, name='correlator')
+		correlator.start(logfile='correlator.log', config=os.path.join(PROJECT.TEST_SUBJECT_DIR,'initialization.yaml'), Xclock=True)
+		correlator.injectEPL(filenames='testUtils.mon', filedir=PROJECT.UTILS_DIR)
+		
+		# Start test results receiver
+		correlator.receive(filename='TestResult.evt', channels=['TestResult'], logChannels=True)
+		
+		# Inject test
+		correlator.injectEPL(filenames=['test.mon'])
+		
+		# Inject timestamps
+		correlator.send(filenames=['timestamps.evt'])
+		
+		# wait for all events to be processed
+		correlator.flush()
+		
+		# wait for test to complete
+		self.waitForSignal('TestResult.evt', expr="TestComplete", condition="==1", timeout=1)
+		
+	def validate(self):
+		# check the main correlator log for Errors
+		self.assertGrep('correlator.log', expr=' (ERROR|FATAL) ', contains=False)
+		
+		# Check that the test didn't fail
+		self.assertGrep('TestResult.evt', expr='TestFailed', contains=False)
+		
