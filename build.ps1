385551e8e6e3cc1b1eb83abe34fbec6288a620b6
Richard Peach (RPEA) <Richard.Peach@softwareag.com>
2018-04-25 12:09:08 +0100

    Adding license info

--- build.ps1
+++ build.ps1
@@ -1,3 +1,17 @@
+# Copyright 2018 Software AG
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 param (
    [string]$sagInstallDir = (.\misc\getSagInstallDir),
    [string]$output = "$(Split-Path $MyInvocation.MyCommand.Path -Parent)\output\RxEPL"
@@ -42,6 +56,8 @@ mv "$output\misc\deploy.bat" "$output\deploy.bat"
 
 # Write out utf8 (no BOM)
 [IO.File]::WriteAllLines("$output\version.txt", $version)
+
+cp -r "$PSScriptRoot\LICENSE" "$output\LICENSE"
 
 # Zip
 if (Get-Command Compress-Archive -errorAction SilentlyContinue) {
