9c964da721a8af71d1cfd0871fc1720fd07caac4
Richard Peach (RPEA) <Richard.Peach@softwareag.com>
2018-05-18 15:09:00 +0100

    Adding code branching coverage information

--- travis-scripts/coverage.ps1
+++ travis-scripts/coverage.ps1
@@ -19,7 +19,7 @@ param (
 	[string]$coverageFile = "$((Get-Childitem -Path ./test -Include merged.eplcoverage -File -Recurse)[0].FullName)"
 )
 
-$coverageInfo = Get-Content -Path $coverageFile | ConvertFrom-Csv -Header Stage,Line,Block,File | Where {$_.File -like '*/output/*'} | % { $_.Line = [int]$_.Line; $_ } | Sort-Object -Property File,Line,Block,Stage 
+$coverageInfo = Get-Content -Path $coverageFile | ConvertFrom-Csv -Header Stage,Line,Block,File | Where {$_.File -like '*/output/*'} | % { $_.Line = [int]$_.Line; $_.Block = [int]$_.Block; $_ } | Sort-Object -Property File,Line,Block,Stage 
 
 $coveredBlocks = @()
 $uncoveredBlocks = @()
@@ -27,7 +27,7 @@ $uncoveredBlocks = @()
 For ($i=0; $i -lt $coverageInfo.length; $i++) {
 	$coverageLine = $coverageInfo[$i]
 	If ($coverageLine.Stage -eq "CODE") {
-		If (($i + 1 -eq $coverage.length) -or ($coverageInfo[$i + 1].Stage -eq "CODE")) {
+		If ((($i + 1) -ge $coverageInfo.length) -or ($coverageInfo[$i + 1].Stage -eq "CODE")) {
 			$uncoveredBlocks += $coverageLine
 		} else {
 			$coveredBlocks += $coverageLine
@@ -35,40 +35,42 @@ For ($i=0; $i -lt $coverageInfo.length; $i++) {
 	}
 }
 
-$lineHitCount = $coveredBlocks | Group-Object Line,File | %{
+$blockHits = $coveredBlocks | Group-Object Line,File,Block | %{
     New-Object psobject -Property @{
         Line = $_.Group[0].Line
 		File = $_.Group[0].File
-        Count = $_.Count
+		Block = $_.Group[0].Block
+		# There's no block hit count in eplcoverage so we'll assume it was hit once
+        Count = 1
     }
 }
-
-$lineHitCount += $uncoveredBlocks | Group-Object Line,File | %{
+$blockHits += $uncoveredBlocks | Group-Object Line,File,Block | %{
     New-Object psobject -Property @{
         Line = $_.Group[0].Line
 		File = $_.Group[0].File
+		Block = $_.Group[0].Block
         Count = 0
     }
 }
-
-$lineHitCount = $lineHitCount | Group-Object File,Line | %{
-    New-Object psobject -Property @{
-        Line = $_.Group[0].Line
-		File = $_.Group[0].File
-        Count = ($_.Group | Measure-Object Count -Sum).Sum
-    }
-} | Sort-Object -Property File,Line
+$blockHits = $blockHits | Sort-Object -Property File,Line,Block
 
 $jsonCoverage = New-Object psobject -Property @{
 	service_job_id = $travisJobId
 	service_name = "travis-ci"
-	source_files = $lineHitCount | Group-Object -Property File | %{
+	source_files = $blockHits | Group-Object File | %{
+		$fileName = $_.Name
 		New-Object psobject -Property @{
 			name = Resolve-Path -Relative $_.Name | %{$_ -replace "\\","/"} | %{$_ -replace ".*/output/.*/code","src"}
 			source_digest = (Get-FileHash $_.Name -Algorithm MD5).Hash
 			coverage = $(
+				$lineHits = $blockHits | Where {$_.File -eq $fileName} | Group-Object Line | %{	
+					New-Object psobject -Property @{
+						Line = $_.Group[0].Line
+						Count = ($_.Group | Measure-Object -Maximum Count).Maximum
+					}
+				}
 				$coverageArray = @()
-				Foreach ($coverageLine in $_.Group) {
+				Foreach ($coverageLine in $lineHits) {
 					While($coverageArray.length -lt ($coverageLine.Line - 1)) {
 						$coverageArray += $null
 					}
@@ -76,8 +78,16 @@ $jsonCoverage = New-Object psobject -Property @{
 				}
 				$coverageArray
 			)
+			# There's no branch information in eplcoverage - we can aproximate this with block coverage:
+			# If one block has been hit but another hasn't (on the same line) then we've missed at least one branch
+			branches = $_.Group | Group-Object Line | Where {(($_.Group | %{$_.Count -gt 0}) -contains $true) -and (($_.Group | %{$_.Count -eq 0}) -contains $true)} | %{ 
+				@(
+					$_.Group[0].Line, 0, 0, 1, 
+					$_.Group[0].Line, 0, 1, 0
+				)
+			}
 		}
 	}
-} | ConvertTo-Json -Depth 10
+} | ConvertTo-Json -Depth 10 -Compress
 
 Invoke-WebRequest -Uri https://coveralls.io/api/v1/jobs -Method POST -Body @{json=$jsonCoverage}
